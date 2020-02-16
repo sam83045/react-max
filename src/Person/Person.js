540fcaee54f7995d6252c4b1073ba87d36daa781
sam83045 <shoaib786k@gmail.com>
2020-02-16 10:51:41 +0530

    added semantic react

---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Button} from "semantic-ui-react"
+
+const person = () => {
+    return (
+        <React.Fragment>
+            <p>I'm a Person</p>
+            <Button primary>Hi</Button>
+        </React.Fragment>
+    )
+}
+
+export default person;
